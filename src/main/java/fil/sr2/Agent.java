fcbf32806397fdbdb18357847b512b32e83f7dcd
El Ammari Nordine <elammari@a11p26.fil.univ-lille1.fr>
2021-04-26 16:33:18 +0200

    modification mineure

--- src/main/java/fil/sr2/Agent.java
+++ src/main/java/fil/sr2/Agent.java
@@ -2,6 +2,7 @@ package fil.sr2;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -9,29 +10,41 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 
 public class Agent {
 
-        public static final String BASE_URI = "http://localhost:8080/flopboxApp/";
+        public static final String BASE_URI = "http://localhost:8080/flopboxApp/servers/";
 
         public String addServer(String alias, String host, Integer port) throws Exception {
 
             CloseableHttpClient httpClient = HttpClients.createDefault();
             URIBuilder builder = new URIBuilder(BASE_URI+alias);
             builder.setParameter("address", host).setParameter("port", Integer.toString(port));
-            return requestAsString(httpClient, builder);
+            return PostRequestAsString(httpClient, builder);
         }
 
         public String listServers() throws Exception{
             CloseableHttpClient httpClient = HttpClients.createDefault();
-            URIBuilder builder = new URIBuilder(BASE_URI+"servers");
-            return requestAsString(httpClient, builder);
+            URIBuilder builder = new URIBuilder(BASE_URI);
+            return GetRequestAsString(httpClient, builder);
         }
 
-    private String requestAsString(CloseableHttpClient httpClient, URIBuilder builder) throws URISyntaxException, IOException {
+    private String PostRequestAsString(CloseableHttpClient httpClient, URIBuilder builder) throws URISyntaxException, IOException {
         HttpPost request = new HttpPost(builder.build());
+        CloseableHttpResponse response = httpClient.execute(request);
+        HttpEntity entity = response.getEntity();
+        String result = "None";
+        if (entity != null) {
+            result = EntityUtils.toString(entity);
+        }
+        return result;
+    }
+
+    private String GetRequestAsString(CloseableHttpClient httpClient, URIBuilder builder) throws URISyntaxException, IOException {
+        HttpGet request = new HttpGet(builder.build());
         CloseableHttpResponse response = httpClient.execute(request);
         HttpEntity entity = response.getEntity();
         String result = "None";
--- src/main/java/fil/sr2/Main.java
+++ src/main/java/fil/sr2/Main.java
@@ -1,27 +1,13 @@
 package fil.sr2;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main( String[] args ) {
         Agent ag = new Agent();
         try {
             ag.addServer("ubuntu", "ftp.ubuntu.com", 21);
-            System.out.println(ag.addServer("free", "ftp.free.fr", 21));
+            ag.addServer("free", "ftp.free.fr", 21);
             System.out.println(ag.listServers());
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpGet r = new HttpGet("http://localhost:8080/flopboxApp/servers");
-            CloseableHttpResponse response = httpClient.execute(r);
-            Scanner sc = new Scanner(response.getEntity().getContent());
-            while(sc.hasNext()) {
-                System.out.println(sc.nextLine());
-            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
